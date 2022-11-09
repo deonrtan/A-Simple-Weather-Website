b6d68bab9a3ee3be9ee134bb181bff7eb6df6d58
Omori <112740690+deonrtan@users.noreply.github.com>
2022-11-09 22:36:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,16 @@ let weather= {
 
         const { name } = data;
         const { icon , description } = data.weather[0];
-        const { temp, humidity } = data.main;
+        const { temp, humidity, } = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed)
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src ="https://openweathermap.org/img/wn/"+icon+".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp +"°C";
         document.querySelector(".humidity").innerText = "humidity: " + humidity +"%";
         document.querySelector(".wind").innerText = "Wind speed: " + speed +" km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?landspace?" + name+"')"
     },
     search: function(){
         this.fetchWeather(document.querySelector(".search-bar").value);
@@ -29,4 +30,12 @@ let weather= {
 
 document.querySelector(".search button").addEventListener("click", function (){
     weather.search();
-})
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if (event.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Philippines");
